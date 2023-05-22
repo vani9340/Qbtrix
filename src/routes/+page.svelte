8f9778e7f23954505ef147532bed4b8bcff2e2fe
vani9340 <sg5428702@gmail.com>
2023-05-22 20:20:30 +0530

    c

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,6 +4,7 @@
       color: white;
       font-family: 'Noto Sans', 'sans-serif';
     }
+
   </style> 
   <script>
     	import Layout from './+layout.svelte';
@@ -13,6 +14,14 @@
     import zoom from '../images/zoom.svg'
     import microsoft from '../images/microsoft.svg'
     import google from '../images/google.svg'
+    import img1 from '../images/1.svg'
+    import img2 from '../images/2.svg'
+    import img3 from '../images/3.svg'
+    import img4 from '../images/4.svg'
+    import img5 from '../images/5.svg'
+    import img6 from '../images/6.svg'
+    import img7 from '../images/7.svg'
+
   </script>
 
 
@@ -51,6 +60,12 @@
 </div>
 
 
+
+
+
+
+
+
 <!-- part-2 -->
 <div>
   <div class='text-center'>
@@ -58,13 +73,17 @@
     <p class='text-[#AEAEAE]'>Lörem ipsum hexavugon selig. Vungen köledes de antisade. Dik tir feskap yrkessåpa i dinyssade. Sysk lufav decirigon fast nehet. Järade tik. Nyvungen paska, det vill säga nynesamma. Vases. Sor tibyska de paralig, syvudäskade polyn. Hundvissla vivis en ede, till previs, som decikonade. Yrinde an kroktiga miplavis orade. Serade vuskade, även om tänott i prenar. Lytt pimpa. </p>
   </div>
 </div>
+
+
+
 <!-- box design -->
-<div>
+<div class='flex gap-[24px] px-[10.4166vw]'>
   <!-- our services -->
-  <div class='max-w-[655px] min-h-[253px]'>
+  <div>
+  <div class='max-w-[655px] min-h-[253px] bg-gradient-to-t from-[#252525] to-[#0B0B0B] border-[#414141] border-[1px] rounded-[24px] flex justify-between'>
     <div>
       <h1>Our Services</h1>
-      <button>Services</button>
+      <button class='rounded-full border-white border-[1px]'>Services</button>
     </div>
     <div>
       <div>
@@ -75,11 +94,58 @@
     </div>
     <div>
       <h1>Mobile Application</h1>
-      <p class='text-[#E4E4E4] bg-black'>Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig.</p>
+      <p class='text-[#E4E4E4]'>Lörem ipsum astrobel sar direlig. Kronde est konfoni med kelig.</p>
 
       <!-- <button><img/></button> -->
     </div>
     </div>
   </div>
+<!-- end of services.. -->
+<!-- new box project -->
+<div class='flex justify-around mt-[20px] items-center max-w-[655px] min-h-[86px] bg-gradient-to-b from-[#101010] to-[#242424] border-[#414141] border-[1px] rounded-[24px]'>
+<div>
+  <h1 class='text-center'>200+</h1>
+  <p>Complite Project</p>
+</div>
+<div>
+  <h1 class='text-center'>1,230</h1>
+  <p>Clietn Trusted</p>
+</div>
+<div>
+  <h1 class='text-center'>1,230</h1>
+  <p>Clietn Trusted</p>
+</div>
+</div>
+
 
 </div>
+<!-- end of two boxes -->
+<!-- who we are box -->
+<div class='flex flex-col justify-center items-start max-w-[461px] min-h-[359px] rounded-[24px] bg-gradient-to-b from-[#101010] to-[#242424] border-[#414141] border-[1px]'>
+<h1>Who We are</h1>
+<p class='text-[#AEAEAE]'>
+  Lörem ipsum hexavugon selig. Vungen köledes de antisade. Dik tir feskap yrkessåpa i dinyssade. Sysk lufav decirigon fast nehet. Järade tik. Nyvungen paska, det vill säga nynesamma. Vases. Sor tibyska de paralig, syvudäskade polyn. Hundvissla vivis en ede, till previs, som decikonade. Yrinde an kroktiga miplavis orade. Serade vuskade, även om tänott i prenar. Lytt pimpa. 
+</p>
+<button class='px-[2rem] py-[14px]  rounded-full bg-gradient-to-b from-[#252525] to-[#101010] to-[100%] border-[#414141]'>
+About Us
+</button>
+</div>
+<!-- languages -->
+
+</div>
+
+<div class=' mx-[10.4166vw] flex justify-evenly max-w-[1140px] min-h-[120px] bg-gradient-to-b from-[#101010]  to-[#242424] to-[100%] border-[#414141] border-[1px] rounded-[24px]'>
+  <img src={img1} alt='1'/>
+  <img src={img2} alt='1'/>
+  <img src={img3} alt='1'/>
+  <img src={img4} alt='1'/>
+  <img src={img5} alt='1'/>
+  <img src={img6} alt='1'/>
+  <img src={img7} alt='1'/>
+
+</div>
+
+<!-- footer -->
+
+
+
---
+++ src/routes/footer/+page.svelte
@@ -0,0 +1,35 @@
+<style>
+
+.btn-gradient-2 {
+  background: linear-gradient(#101010,#242424) padding-box,
+              linear-gradient(to right, #F2531E 0%, #FACF4B 18.23%, #7ACE46 41.67%, #5392FA 60.42%, #785EFA 80.73%, #F64CD5 100%) border-box;
+  border-radius: 24px;
+  border: 6px solid transparent;
+
+}
+/* .btn-gradient-1 {
+   
+    border-image-source: linear-gradient(white, white) padding-box,
+    linear-gradient(90deg, #F2531E 0%, #FACF4B 18.23%, #7ACE46 41.67%, #5392FA 60.42%, #785EFA 80.73%, #F64CD5 100%) border-box;
+    border-width: 6px;
+    border-style: solid;
+    border-radius: 24px;
+} */
+
+</style>
+
+
+
+<!-- Result Skip Results Iframe
+EDIT ON -->
+<div class="parent">
+  <div class="btn-gradient-2 max-w-[1126px]  min-h-[197px] ">  <div>
+    <h1>
+        Start Project With Qbtrix
+    </h1>
+    <p>
+        Lörem ipsum bira homomäktiga såsom jynde eurol ijesa. Kontos fiska astrovision. Öfäsm neligt medeltologi: medan vatäning....
+    </p>
+</div>
+<button>Start a project</button></div>
+</div>
